bd6342b4f472f80e08f280b085dd88fda122a0d4
markosthabit <markosthabit@protonmail.com>
2025-07-22 05:47:28 +0300

    ✨feat(user):  add user services.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaClient } from '../../generated/prisma';
+
+const prisma = new PrismaClient();
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
+  //TODO
+  async create(createUserDto: CreateUserDto) {
+    await prisma.user.create({});
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    return await prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await prisma.user.findFirst({ where: { id: `${id}` } });
+    if (!user) throw new NotFoundException('User Not Found');
+    return user;
   }
 
+  //TODO
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    await prisma.user.delete({ where: { id: `${id}` } });
+    return { message: 'deleted successfully' };
   }
 }
